322bd91bbadf4d8f2cd2c4c8a473dbf143f006fc
Tomasz Kucharczyk <215786@edu.p.lodz.pl>
2020-11-17 15:13:26 +0100

    Connection to calendar added

--- src/main/java/com/PPKWU/ZAD3/Downloader.java
+++ src/main/java/com/PPKWU/ZAD3/Downloader.java
@@ -1,12 +1,19 @@
 package com.PPKWU.ZAD3;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 @Service
 public class Downloader {
-    public List<String> Download(String year, String month) {
-        
+    public List<String> Download(String year, String month) throws IOException {
+        Connection connection = Jsoup.connect("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php"
+                                              + "?rok=" + year +"?month=" + month);
+        return Collections.emptyList();
     }
 }
