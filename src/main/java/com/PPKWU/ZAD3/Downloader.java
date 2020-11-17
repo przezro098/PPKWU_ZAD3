0ad613f4cb784defe86435f416b0af161c235392
Tomasz Kucharczyk <215786@edu.p.lodz.pl>
2020-11-17 15:09:10 +0100

    Added Jsoup dependency

--- src/main/java/com/PPKWU/ZAD3/Downloader.java
+++ src/main/java/com/PPKWU/ZAD3/Downloader.java
@@ -2,7 +2,11 @@ package com.PPKWU.ZAD3;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class Downloader {
-
+    public List<String> Download(String year, String month) {
+        
+    }
 }
