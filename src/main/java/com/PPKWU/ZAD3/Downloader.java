58d878bf999b8be56f2fbe5faab3ced234d78ec6
Tomasz Kucharczyk <215786@edu.p.lodz.pl>
2020-11-17 15:29:58 +0100

    Algorythm for days and eventnames downloading added

--- src/main/java/com/PPKWU/ZAD3/Downloader.java
+++ src/main/java/com/PPKWU/ZAD3/Downloader.java
@@ -3,17 +3,31 @@ package com.PPKWU.ZAD3;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 @Service
 public class Downloader {
     public List<String> Download(String year, String month) throws IOException {
+        List<String> events = new ArrayList<>();
         Connection connection = Jsoup.connect("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php"
                                               + "?rok=" + year +"?month=" + month);
-        return Collections.emptyList();
+
+        Elements days = connection.get().select("a.active");
+        Elements eventNames = connection.get().select("div.InnerBox");
+
+        for (int i=0;i<days.size();i++ ) {
+            String day = days.get(i).text();
+            String name = eventNames.get(i).text();
+            events.add(day+ " " + name);
+        }
+
+        return events;
     }
 }
