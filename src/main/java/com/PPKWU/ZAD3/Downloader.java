4b1bb4c1c7b36557488a7a50d8f3cbbffed38bf9
Tomasz Kucharczyk <215786@edu.p.lodz.pl>
2020-12-01 14:34:44 +0100

    Added Ical parser algorythm

--- src/main/java/com/PPKWU/ZAD3/Downloader.java
+++ src/main/java/com/PPKWU/ZAD3/Downloader.java
@@ -1,6 +1,7 @@
 package com.PPKWU.ZAD3;
 
 import biweekly.ICalendar;
+import biweekly.component.VEvent;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -10,12 +11,14 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
 
 @Service
 public class Downloader {
     public List<String> Download(String year, String month) throws IOException {
+        //OBSOLETE
         //List<String> events = new ArrayList<>();
         Connection connection = Jsoup.connect("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php"
                                               + "?rok=" + year +"?month=" + month);
@@ -24,7 +27,7 @@ public class Downloader {
         Elements eventNames = connection.get().select("div.InnerBox");
 
         /*
-
+        OBSOLETE
         for (int i=0;i<days.size();i++ ) {
             String day = days.get(i).text();
             String name = eventNames.get(i).text();
@@ -33,6 +36,21 @@ public class Downloader {
 
          */
         ICalendar iCalendar = new ICalendar();
+        for (int i=0;i<days.size();i++)
+        {
+            Calendar calendar = Calendar.getInstance();
+            String day = days.get(i).text();
+            String name = eventNames.get(i).text();
+            calendar.set(Integer.parseInt(year),Integer.parseInt(month),Integer.parseInt(day));
+
+            VEvent vEvent = new VEvent();
+            vEvent.setSummary(name);
+            vEvent.setDateTimeStamp(calendar.getTime());
+
+            iCalendar.addEvent(vEvent);
+
+        }
+
 
 
         return ;
